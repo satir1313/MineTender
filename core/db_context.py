1d88c7aad40b5fd85e29856a28820efd04a26d54
shahab <shahabj13@gmail.com>
2023-02-10 20:01:16 +0800

    update requirements

--- core/db_context.py
+++ core/db_context.py
@@ -3,28 +3,23 @@ from dotenv import load_dotenv
 import pyodbc
 
 class db_context:
+    db_connection = None
+    cursor = None
     def __init__(self):
         load_dotenv()
-        pass
-
-    def db_connect(self):
         try:
             connection_strings = os.getenv('CONNECTION_STRINGS')
-            connection = pyodbc.connect(connection_strings)
-            self.create_database(connection)
-            return True
+            self.db_connection = pyodbc.connect(connection_strings)
+            self.cursor = self.db_connection.cursor()
         except:
             print("Connection to databse failed!")
-            return False
-
 
     ### create reuired database in the first run the app
-    def create_database(self, connection):
-        try:
-            cursor = connection.cursor()
-            exists = self.check_table_exists(connection, "Equipment")
+    def create_database(self):
+        try:           
+            exists = self.check_table_exists("Equipment")
             if not exists:
-                cursor.execute("""CREATE TABLE Equipment (
+                self.cursor.execute("""CREATE TABLE Equipment (
                                 id INT NOT NULL,
                                 name VARCHAR(255),
                                 brand VARCHAR(255),
@@ -32,33 +27,38 @@ class db_context:
                                 capacity DECIMAL,
                                 concumption DECIMAL,
                                 PRIMARY KEY(id));""")
-                cursor.commit()
+                self.cursor.commit()
                 print("Database is created!")
             else:
                 print('Database already exists')
                 # close connection anyway
-                connection.close()
+                self.db_connection.close()
         except:
             print("Database is not created!")
             
     ### check whether table already exist in the database        
-    def check_table_exists(self, dbcon, tablename):
-        dbcur = dbcon.cursor()
-        dbcur.execute("""
+    def check_table_exists(self, tablename):
+        self.cursor.execute("""
             SELECT COUNT(*)
             FROM information_schema.tables
             WHERE table_name = '{0}'
             """.format(tablename.replace('\'', '\'\'')))
-        if dbcur.fetchone()[0] == 1:
-            dbcur.close()
+        if self.cursor.fetchone()[0] == 1:
+            self.cursor.close()
             return True
 
-        dbcur.close()
+        self.cursor.close()
         return False
 
+    def db_insert(self):
+        self.cursor.execute("""INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+                                VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway'); """)
+
+        
+        
 
 db = db_context()
-db.db_connect()
+db.create_database()
 
 # if __name__ == "__main__":   
 #     db = db_context()
